7f29bbf24fc705c268cb64b074953cead43f6564
klholton <kholton3@jhu.edu>
2023-04-19 01:45:58 -0400

    Receiver.cpp

--- receiver.cpp
+++ receiver.cpp
@@ -19,15 +19,50 @@ int main(int argc, char **argv) {
   std::string room_name = argv[4];
 
   Connection conn;
+  rio_t rio;
 
-  // TODO: connect to server
 
+  // connect to server
+  conn.connect(server_hostname, server_port);
+  
   // TODO: send rlogin and join messages (expect a response from
   //       the server for each one)
+  Message* msg = new Message(TAG_RLOGIN, username);
+  conn.send(*msg);
 
+  // expect an "ok" message
+  Message msg3 = Message(TAG_OK, "You're in!");
+
+  // rlogin messages
+  if (conn.receive(msg3)) { 
+    // register receiver thread with username
+  } else {
+    Message* msg = new Message(TAG_ERR, "Failed to connect to server");
+    Rio_writen(STDERR_FILENO, msg->msg.c_str(), msg->datasize);
+    return 1; // Exit with non-zero code
+  }
+
+  Message* msg1 = new Message(TAG_JOIN , room_name);
+  conn.send(*msg1);
+
+  // join messages
+  if (conn.receive(msg3)) { 
+    // register receiver to room
+  } else {
+    Message* msg1 = new Message(TAG_ERR, "Failed to join the room");
+    Rio_writen(STDERR_FILENO, msg1->msg.c_str(), msg1->datasize);
+    return 1; // Exit with non-zero code
+  }
+
+  std::string currentLine;
+  
   // TODO: loop waiting for messages from server
   //       (which should be tagged with TAG_DELIVERY)
+  while (1) {
+    
+    rio_readlineb(rio, void *usrbuf, RIO_BUFSIZE);
 
+  }
 
   return 0;
 }
