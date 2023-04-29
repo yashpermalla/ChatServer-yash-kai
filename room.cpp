cdf7466c552caef4d2a8254a9a49c99877ec9c9d
Yash <yashpermalla@gmail.com>
2023-04-29 16:48:03 -0400

    guard stuff

--- room.cpp
+++ room.cpp
@@ -18,21 +18,21 @@ Room::~Room() {
 
 void Room::add_member(User *user) {
   // add User to the room
-  Guard(Room::lock);
+  Guard g(lock);
   Room::members.insert(user);
   // Note: should unlock when Guard goes out-of-scope!
 }
 
 void Room::remove_member(User *user) {
   // remove User from the room
-  Guard(Room::lock);
+  Guard g(lock);
   Room::members.erase(user);
   // Note: should unlock when Guard goes out-of-scope!
 }
 
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
   // send a message to every (receiver) User in the room
-  Guard(Room::lock);
+  Guard g(lock);
   for (User* user : members) {
     Message msg = Message(sender_username, message_text);
     user->mqueue.enqueue(&msg);
