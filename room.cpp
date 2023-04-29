53f8bb69008bd2df67b7d1a3fd368ecbe033f385
klholton <kholton3@jhu.edu>
2023-04-29 16:20:10 -0400

    Finished room.cpp :)

--- room.cpp
+++ room.cpp
@@ -4,23 +4,38 @@
 #include "user.h"
 #include "room.h"
 
+
 Room::Room(const std::string &room_name)
   : room_name(room_name) {
-  // TODO: initialize the mutex
+  // initialize the mutex
+  pthread_mutex_init(&lock, NULL);
 }
 
 Room::~Room() {
-  // TODO: destroy the mutex
+  // destroy the mutex
+  pthread_mutex_destroy(&lock);
 }
 
 void Room::add_member(User *user) {
-  // TODO: add User to the room
+  // add User to the room
+  Guard(Room::lock);
+  Room::members.insert(user);
+  // Note: should unlock when Guard goes out-of-scope!
 }
 
 void Room::remove_member(User *user) {
-  // TODO: remove User from the room
+  // remove User from the room
+  Guard(Room::lock);
+  Room::members.erase(user);
+  // Note: should unlock when Guard goes out-of-scope!
 }
 
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
-  // TODO: send a message to every (receiver) User in the room
+  // send a message to every (receiver) User in the room
+  Guard(Room::lock);
+  for (User* user : members) {
+    Message msg = Message(sender_username, message_text);
+    user->mqueue.enqueue(&msg);
+  }
+  // Note: should unlock when Guard goes out-of-scope!
 }
